6f9ad223c5d248a5cc312e2eb7d7750e85bbecaf
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2020-01-05 23:07:13 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  // the moment the user disconnects, this will be triggered.
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  })
+
+});
+
+
+
+http.listen(3000, () => {
+  console.log('listening on http://localhost:3000');
+});
